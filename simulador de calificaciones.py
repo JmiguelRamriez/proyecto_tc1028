21434817eeaf0f5f6d0f41df4f8ce081b3c30743
JmiguelRamriez <A01712628@tec.mx>
2024-08-12 20:58:27 -0600

    Create simulador de calificaciones.py

---
+++ simulador de calificaciones.py
@@ -0,0 +1,20 @@
+//Revisar calificaciones de estudiante
+
+import random
+
+acticidad1 = act1;  //con esto asignamos variables para cada una de las actividades 
+acticidad2 = act2;
+acticidad3 = act3;
+acticidad4 = act4;
+acticidad5 = act5;
+
+nota_final = ((act1 + act2 + act3 + act4 + act5)/5);
+
+print ("ingrese la calificación que le gustaria tener: "), act1       //aqui se ingresa los datos que estan asignados a cada variable 
+print ("ingrese la calificación que le gustaria tener: "), act2
+print ("ingrese la calificación que le gustaria tener: "), act3
+print ("ingrese la calificación que le gustaria tener: "), act4
+print ("ingrese la calificación que le gustaria tener: "), act5
+
+print ("La nota que usted va a tener para el final del semestre es de: "), nota_final   //aqui podemos ver el posible resultado que queremos obtener para el final del semestre
+
