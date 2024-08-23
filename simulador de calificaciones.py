2415a8434ee9cab1e6130e20c48b925e2960c365
JmiguelRamriez <A01712628@tec.mx>
2024-08-23 17:10:14 -0600

    Update simulador de calificaciones.py

--- simulador de calificaciones.py
+++ simulador de calificaciones.py
@@ -1,20 +1,26 @@
 //Revisar calificaciones de estudiante
 
-import random
 
-acticidad1 = act1;  //con esto asignamos variables para cada una de las actividades 
-acticidad2 = act2;
-acticidad3 = act3;
-acticidad4 = act4;
-acticidad5 = act5;
+print('El total de todas las actividades equivale al 70% de la calificacion final')
+print('El examen equivale al 30% de la calificacion final')
 
-nota_final = ((act1 + act2 + act3 + act4 + act5)/5);
+def simulador_calf ():
+    return () 
 
-print ("ingrese la calificación que le gustaria tener: "), act1       //aqui se ingresa los datos que estan asignados a cada variable 
-print ("ingrese la calificación que le gustaria tener: "), act2
-print ("ingrese la calificación que le gustaria tener: "), act3
-print ("ingrese la calificación que le gustaria tener: "), act4
-print ("ingrese la calificación que le gustaria tener: "), act5
+def notas ():   
+    act1 = float(input('Ingrese su calificacion de la actividada 1: '))
+    act2 = float(input('Ingrese su calificacion de la actividada 2: '))
+    act3 = float(input('Ingrese su calificacion de la actividada 3: '))
+    act4 = float(input('Ingrese su calificacion de la actividada 4: '))
+    exam = float(input('Ingrese su calificaion en el examen: '))
 
-print ("La nota que usted va a tener para el final del semestre es de: "), nota_final   //aqui podemos ver el posible resultado que queremos obtener para el final del semestre
+    actividades = 0.70
+    examen = 0.30
 
+    actividades = (act1 + act2 + act3 + act4 )/4
+    nota_final = actividades + examen
+    print('la nota final es :', nota_final)
+    return (nota_final)
+
+
+notas ()
