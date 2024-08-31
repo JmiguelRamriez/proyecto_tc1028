33adc0268513753819f991a6c8e210a69d2b4ae8
JmiguelRamriez <A01712628@tec.mx>
2024-08-31 00:00:27 -0600

    Update simulador de calificaciones.py

--- simulador de calificaciones.py
+++ simulador de calificaciones.py
@@ -1,26 +1,23 @@
 //Revisar calificaciones de estudiante
 
 
-print('El total de todas las actividades equivale al 70% de la calificacion final')
-print('El examen equivale al 30% de la calificacion final')
+def simulador_calf():
+    print('El total de todas las actividades equivale al 70% de la calificación final')
+    print('El examen equivale al 30% de la calificación final')
+    return simulador_calf
 
-def simulador_calf ():
-    return () 
+def notas():
+    act1 = float(input('Ingrese su calificación de la actividad 1: '))
+    act2 = float(input('Ingrese su calificación de la actividad 2: '))
+    act3 = float(input('Ingrese su calificación de la actividad 3: '))
+    act4 = float(input('Ingrese su calificación de la actividad 4: '))
+    exam = float(input('Ingrese su calificación en el examen: '))
 
-def notas ():   
-    act1 = float(input('Ingrese su calificacion de la actividada 1: '))
-    act2 = float(input('Ingrese su calificacion de la actividada 2: '))
-    act3 = float(input('Ingrese su calificacion de la actividada 3: '))
-    act4 = float(input('Ingrese su calificacion de la actividada 4: '))
-    exam = float(input('Ingrese su calificaion en el examen: '))
+    actividades = 0.70 * ((act1 + act2 + act3 + act4) / 4)
+    examen = 0.30 * exam
 
-    actividades = 0.70
-    examen = 0.30
-
-    actividades = (act1 + act2 + act3 + act4 )/4
     nota_final = actividades + examen
-    print('la nota final es :', nota_final)
-    return (nota_final)
+    print('La nota final es:', nota_final)
+    return nota_final
 
-
-notas ()
+notas()
